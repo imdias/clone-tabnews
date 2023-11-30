b8ccfe6ace41f196ffb2e2e90f9bb637d14614a8
Igor Dias <56363119+imdias@users.noreply.github.com>
2023-11-30 03:03:31 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Implementação do http://www.tabnews.com.br para o http://curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home() {
-    return <h1>Bru, finalmente estou aprendendo a programar! </h1>
+  return <h1>Bru, finalmente estou aprendendo a programar! </h1>;
+}
+
+function teste2() {
+  console.log("identtação errada");
 }
 
 export default Home;
