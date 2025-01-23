3280f96bfb611bafec1bacfa3ac7c1c0b17e2dba
Igor Dias <56363119+imdias@users.noreply.github.com>
2025-01-23 03:42:07 +0000

    handles database ssl connection

--- infra/database.js
+++ infra/database.js
@@ -7,6 +7,7 @@ async function query(queryObjective) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
+    ssl: process.env.NODE_ENV === "development" ? false : true,
   });
 
   console.log("Credenciais do Postgres:", {
