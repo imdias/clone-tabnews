a63aa85e45d039d4d426dc038464b057b11a5dba
Igor Dias <56363119+imdias@users.noreply.github.com>
2025-01-26 18:00:54 +0000

    correção ternário para resolução do erro 500 em produção

--- infra/database.js
+++ infra/database.js
@@ -7,7 +7,7 @@ async function query(queryObjective) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-    ssl: process.env.NODE_ENV === "development" ? false : true,
+    ssl: process.env.NODE_ENV === "development" ? true : false,
   });
 
   console.log("Credenciais do Postgres:", {
